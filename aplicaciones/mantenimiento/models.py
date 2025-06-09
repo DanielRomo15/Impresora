87f8ae00d00da3931f3c71bcc72466f0c750d1b6
DanielRomo15 <137075635+DanielRomo15@users.noreply.github.com>
2025-06-09 09:16:28 -0500

    Update models.py

--- aplicaciones/mantenimiento/models.py
+++ aplicaciones/mantenimiento/models.py
@@ -4,20 +4,7 @@ from django.db import models
 
 #TABLAS
 
-class Impresora(models.Model):
-    ESTADO_CHOICES = [
-        ('en_proceso', 'En Proceso'),
-        ('finalizado', 'Finalizado'),
-    ]
 
-    id = models.AutoField(primary_key=True)
-    marca = models.CharField(max_length=50)
-    propietario = models.CharField(max_length=100)
-    descripcion = models.TextField()
-    estado = models.CharField(max_length=15, choices=ESTADO_CHOICES, default='en_proceso')
-
-    def __str__(self):
-        return f"{self.marca} - {self.propietario} ({self.estado})"
 
 
 class Mantenimiento(models.Model):
