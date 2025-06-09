2947a0daa08f16bb012d1d7463d0133454d3f0dc
DanielRomo15 <137075635+DanielRomo15@users.noreply.github.com>
2025-06-09 09:17:00 -0500

    Update models.py
    crear clase impresora

--- aplicaciones/mantenimiento/models.py
+++ aplicaciones/mantenimiento/models.py
@@ -3,15 +3,21 @@ from django.db import models
 # Create your models here.
 
 #TABLAS
+class Impresora(models.Model):
+    ESTADO_CHOICES = [
+        ('en_proceso', 'En Proceso'),
+        ('finalizado', 'Finalizado'),
+    ]
 
-
-
-
-class Mantenimiento(models.Model):
     id = models.AutoField(primary_key=True)
-    costo = models.DecimalField(max_digits=10, decimal_places=2)
-    detalles = models.TextField()
-    impresora = models.ForeignKey(Impresora, on_delete=models.CASCADE, related_name='mantenimientos')
+    marca = models.CharField(max_length=50)
+    propietario = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    estado = models.CharField(max_length=15, choices=ESTADO_CHOICES, default='en_proceso')
 
     def __str__(self):
-        return f"Mantenimiento {self.id} - {self.impresora.marca} ({self.costo} USD)"
+        return f"{self.marca} - {self.propietario} ({self.estado})"
+
+
+
+
