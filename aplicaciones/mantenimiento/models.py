92431d42729e256087a71859e4f75bfc9f2355be
DanielRomo15 <137075635+DanielRomo15@users.noreply.github.com>
2025-06-09 09:17:20 -0500

    Update models.py
    crear clase mentenimiento

--- aplicaciones/mantenimiento/models.py
+++ aplicaciones/mantenimiento/models.py
@@ -19,5 +19,13 @@ class Impresora(models.Model):
         return f"{self.marca} - {self.propietario} ({self.estado})"
 
 
+class Mantenimiento(models.Model):
+    id = models.AutoField(primary_key=True)
+    costo = models.DecimalField(max_digits=10, decimal_places=2)
+    detalles = models.TextField()
+    impresora = models.ForeignKey(Impresora, on_delete=models.CASCADE, related_name='mantenimientos')
+
+    def __str__(self):
+        return f"Mantenimiento {self.id} - {self.impresora.marca} ({self.costo} USD)"
 
 
